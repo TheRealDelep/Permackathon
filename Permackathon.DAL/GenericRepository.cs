f7f28ba35cd0904f3f38727e3ae3451ab98c9385
Sylvain Delépée <sylvain.delepee@gmail.com>
2020-03-09 21:01:41 +0100

    creating a querry using reflection

---
+++ Permackathon.DAL/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Permackathon.DAL
+{
+    public class GenericRepository<T> where T : Entity
+    {
+        private PermaContext context;
+        private DbSet<T> dbSet;
+
+        public GenericRepository(PermaContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return dbSet.
+                Include(x => x.GetType().GetProperties().Where(p => p.GetType().IsClass));
+        }
+    }
+}
