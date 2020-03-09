41b173a7db9308e8597e194a5ad0cff9395812d9
Sylvain Delépée <sylvain.delepee@gmail.com>
2020-03-09 15:16:39 +0100

    Creating DbSets

---
+++ Permackathon.DAL/PermaContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Permackathon.DAL.Entities;
+using System;
+
+namespace Permackathon.DAL
+{
+    public class PermaContext : DbContext
+    {
+        public PermaContext()
+        { }
+
+        public PermaContext(DbContextOptions<PermaContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder is null)
+                throw new ArgumentNullException(nameof(optionsBuilder));
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=FacilityDB;Trusted_Connection=True;");
+                optionsBuilder.EnableSensitiveDataLogging();
+            }
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<MemberRole> MemberRoles { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<State> States { get; set; }
+    }
+}
