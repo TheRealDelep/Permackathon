e6f4d5102f8a9a51c2c2f49f7a0010487945de2b
manssourh <57532271+mans88@users.noreply.github.com>
2020-03-09 21:57:57 +0100

    Creation du Repository de la mort -_- qui tue :D

---
+++ Permackathon.DAL.Tests/Test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Permackathon.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Permackathon.DAL.Tests
+{
+    [TestClass]
+    public class Test
+    {
+       [TestMethod]
+       
+        public void AddInGenericRepositoryTest()
+        {
+            var option = new DbContextOptionsBuilder<PermaContext>()
+                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+                .Options;
+
+            using (var memoryContext = new PermaContext(option))
+            {
+                // Act
+                GenericRepository<Member> genericRepository = new GenericRepository<Member>(memoryContext);
+                //Arrang
+                Member membre = new Member {Email="blabla@mail.com", FirstName="Sponge",LastName="Bob",Password="123456"};
+                genericRepository.Insert(membre);
+                memoryContext.SaveChanges();
+                //Assert
+                var testMember = genericRepository.GetByID(membre.id);
+                Assert.AreEqual("Bob",testMember.LastName);
+
+            }
+        }
+    }
+}
