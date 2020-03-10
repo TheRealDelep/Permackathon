464f8a6220a744cff867ea868319f72c02bc9fb2
dotnet <umuhire.dev@gmail.com>
2020-03-10 10:05:36 +0100

    modidy somme entities

--- Permackathon.DAL/Entities/Site.cs
+++ Permackathon.DAL/Entities/Site.cs
@@ -7,6 +7,6 @@ namespace Permackathon.DAL.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
-        public List<Location> Locations { get; set; }
+        public Location Location { get; set; }
     }
 }
